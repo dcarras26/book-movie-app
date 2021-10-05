c879d0caa26f2bfda26f6e9920e8274a28707d50
Dennis Carrasquillo <spycydennis@gmail.com>
2021-10-04 21:49:32 -0400

    Added logic to the Movie and User Controllers

--- java/src/main/java/com/springangular/practice/java/controllers/MovieController.java
+++ java/src/main/java/com/springangular/practice/java/controllers/MovieController.java
@@ -3,13 +3,17 @@ package com.springangular.practice.java.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springangular.practice.java.exceptions.ResourceNotFoundException;
+import com.springangular.practice.java.models.Book;
 import com.springangular.practice.java.models.Movie;
 import com.springangular.practice.java.repositories.MovieRepository;
 
@@ -31,6 +35,14 @@ public class MovieController {
 	@PostMapping("/movies")
 	public Movie createMovie(@RequestBody Movie movie) {
 		return movieRepository.save(movie);
+	}
+	
+	@CrossOrigin(origins = "http://localhost:4200")
+	@GetMapping("/movies/{id}")
+	public ResponseEntity<Movie> getMovieById(@PathVariable Long id) {
+		Movie movie = movieRepository
+				.findById(id).orElseThrow(()-> new ResourceNotFoundException("The Movie with this id: " + id + " does not exist!"));
+		return ResponseEntity.ok(movie);
 	}
 
 }
